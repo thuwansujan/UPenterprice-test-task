ec6df02bd58b6d76f9d892d8c7c3a3b47b3e56f0
Thuwan <thuwansujan@gmail.com>
2020-12-13 19:40:34 +0530

    Added new template and page

---
+++ wp-content/themes/twentytwenty/templates/template-my-new-template.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: My New Template
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+echo "This is from my new template";
+
+get_template_part( 'singular' );
